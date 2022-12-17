00cc36a5d44e5fe82df19f5a87ce2e6730c7f0ea
Barto17-PL <bartekrzycki@gmail.com>
2022-12-17 18:09:44 +0100

    changed tests 1

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,17 +1,17 @@
 from recipe_api.models import Recipe
 import pytest
 
-def test_create_account():
+def test_create_recipe():
     """
     CHANGE THIS DESCRIPTION!
 
     GIVEN a Account model
-    WHEN a new Account is created
-    THEN check the name, account_number, balance, currency, status and created_at fields are defined correctly
+    WHEN a new Recipe is created
+    THEN check the name, steps, rating, ingredients, favorite fields are defined correctly
     """
     recipe = Recipe('Cake', 'No steps available', 2, 'Carrot, Apple, Butter', True)
     assert recipe.name == 'Cake'
-    assert recipe.steps == '€'
+    assert recipe.steps == 'No steps available'
     assert recipe.rating == 2
     assert recipe.ingredients == 'Carrot, Apple, Butter'
     assert recipe.favorite == True
